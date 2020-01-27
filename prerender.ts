6c107f7a9f6080908eeb70e392d17e4b10fabe5e
manzonif <manzoni.arcadiaweb@gmail.com>
2020-01-27 10:43:24 +0100

    tsc compilation

--- prerender.ts
+++ prerender.ts
@@ -1,3 +1,5 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
 import { environment } from './src/environments/environment';
 
 // for debug
@@ -6,7 +8,10 @@ require('source-map-support').install();
 const domino = require('domino');
 const fs = require('fs');
 const path = require('path');
-const template = fs.readFileSync(path.join(__dirname, '.', 'dist', 'index.html')).toString();
+
+
+// const template = fs.readFileSync(path.join(__dirname, '.', 'dist', 'index.html')).toString();
+const template = fs.readFileSync(path.join(__dirname, '..', 'dist', 'index.html')).toString();
 const win = domino.createWindow(template);
 const files = fs.readdirSync(`${process.cwd()}/dist-server`);
 
@@ -44,14 +49,15 @@ import { ROUTES } from './static.paths';
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const mainFiles = files.filter((file) => file.startsWith('main'));
 const hash = mainFiles[0].split('.')[1];
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require(`./dist-server/main.${hash}`);
+// const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require(`./dist-server/main.${hash}`);
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require(`../dist-server/main.${hash}`);
 import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
 import { NgxRequest, NgxResponce } from '@gorniv/ngx-universal';
 
 const BROWSER_FOLDER = join(process.cwd(), 'static');
 
 // Load the index.html file containing referances to your application bundle.
-const index = readFileSync(join('dist', 'index.html'), 'utf8');
+const index = readFileSync(join('..', 'dist', 'index.html'), 'utf8');
 
 let previousRender = Promise.resolve();
 
